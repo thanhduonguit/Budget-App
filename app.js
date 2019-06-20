b38b6bc43a7d5724937d0f825e49dae735f605de
Duong Tan Thanh <44610939+thanhduonguit@users.noreply.github.com>
2019-06-20 21:57:09 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 // Author: Thanh Dương
 // Ngan sach - Phi Ton - Tien con lai
 class UI {
-  constructor() {
-    this.budgetFeedback = document.querySelector(".budget-feedback")
-    this.budgetForm = document.getElementById("budget-form")
-    this.budgetInput = document.getElementById("budget-input")
-    this.budgetAmount = document.getElementById("budget-amount")
+//   constructor() {
+//     this.budgetFeedback = document.querySelector(".budget-feedback")
+//     this.budgetForm = document.getElementById("budget-form")
+//     this.budgetInput = document.getElementById("budget-input")
+//     this.budgetAmount = document.getElementById("budget-amount")
 
-    this.expenseFeedback = document.querySelector(".expense-feedback")
-    this.expenseAmount = document.getElementById("expense-amount")
-    this.expenseForm = document.getElementById("expense-form")
-    this.expenseInput = document.getElementById("expense-input")
-    this.expenseList = document.getElementById("expense-list")
+//     this.expenseFeedback = document.querySelector(".expense-feedback")
+//     this.expenseAmount = document.getElementById("expense-amount")
+//     this.expenseForm = document.getElementById("expense-form")
+//     this.expenseInput = document.getElementById("expense-input")
+//     this.expenseList = document.getElementById("expense-list")
 
-    this.balance = document.getElementById("balance")
-    this.balanceAmount = document.getElementById("balance-amount")
+//     this.balance = document.getElementById("balance")
+//     this.balanceAmount = document.getElementById("balance-amount")
     
-    this.amountInput = document.getElementById("amount-input")
-    this.itemList = []
-    this.itemID = 0
-  }
+//     this.amountInput = document.getElementById("amount-input")
+//     this.itemList = []
+//     this.itemID = 0
+//   }
 
   // SUBMIT BUDGET
   submitBudgetForm() {
